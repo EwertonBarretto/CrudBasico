f6319ec9f19ca1dd00dcee6eb23a412fa7e843e1
Tom barreto <tom_barreto@hotmail.com>
2018-11-20 18:31:51 -0300

    state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Home from './home/home';
-import CadUser from './Caduser';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./home/home";
+import CadUser from "./Caduser";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Home/>
+        <Home />
       </div>
     );
   }
--- src/home/home.js
+++ src/home/home.js
@@ -1,23 +1,47 @@
-import React, { Component } from 'react';
-import index from '../Caduser/index';
-
+import React, { Component } from "react";
+import index from "../Caduser/index";
+/* eslint-disable */
 
 class home extends Component {
-    constructor(props){
-        super(props);
-        this.cad = index.bind(this);
-    }
+  constructor() {
+    super();
+    this.state = {
+      ListaUsuario: [
+        {
+          nome: "carlinho",
+          email: "c@linhos.com"
+        }
+      ]
+    };
+  }
 
-    render() {
-        return(
-        <div> 
-            <button 
-                onClick={this.cad}>
-                Novo Cadastr
-            </button>
+  render() {
+    return (
+      <div>
+        <div>
+          <button onClick={() => alert("uhuhu")}>Novo Cadastr</button>
         </div>
-        );
-    }
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>Nome</th>
+                <th>email</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.ListaUsuario.map((user: any) => (
+                <tr>
+                  <td>{user.nome}</td>
+                  <td>{user.email}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default home;
